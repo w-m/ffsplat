aae2351c033a65c3eb8f0bfa6c5aff15c0e02288
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2025-02-10 17:20:37 +0100

    type annotations and dependency checks

--- src/examples/load_ply.py
+++ src/examples/load_ply.py
@@ -6,12 +6,24 @@ import numpy as np
 import torch
 import viser
 from gsplat.rendering import rasterization
+from jaxtyping import Float
+from numpy.typing import NDArray
 from plyfile import PlyData
+from torch import Tensor
 
-from view import CameraState, Viewer
+from view.viewer import CameraState, Viewer
 
 
-def load_ply(path):
+def load_ply(
+    path: str,
+) -> tuple[
+    Float[NDArray, "num_gaussians 3"],  # means
+    Float[NDArray, "num_gaussians 4"],  # quats
+    Float[NDArray, "num_gaussians 3"],  # scales
+    Float[NDArray, " num_gaussians"],  # opacities
+    Float[NDArray, "num_gaussians 1 3"],  # sh0
+    Float[NDArray, "num_gaussians S 3"],  # shN
+]:
     data = PlyData.read(path)
     means = np.vstack([data["vertex"]["x"], data["vertex"]["y"], data["vertex"]["z"]]).T
     quats = np.vstack([
@@ -44,33 +56,34 @@ if __name__ == "__main__":
     parser.add_argument("--data_path", type=str)
 
     args = parser.parse_args()
-    means, quats, scales, opacities, sh0, shN = load_ply(args.data_path)
 
-    means = torch.from_numpy(means).to("cuda")
-    quats = torch.from_numpy(quats).to("cuda")
-    scales = torch.from_numpy(scales).to("cuda")
-    opacities = torch.from_numpy(opacities).to("cuda")
-    sh0 = torch.from_numpy(sh0).to("cuda")
-    shN = torch.from_numpy(shN).to("cuda")
-    colors = torch.cat([sh0, shN], dim=-2).to("cuda")
+    means, quats, scales, opacities, sh0, shN = load_ply(args.data_path)
+    means_t: Float[Tensor, "num_gaussians 3"] = torch.from_numpy(means).to("cuda")
+    quats_t: Float[Tensor, "num_gaussians 4"] = torch.from_numpy(quats).to("cuda")
+    scales_t: Float[Tensor, "num_gaussians 3"] = torch.from_numpy(scales).to("cuda")
+    opacities_t: Float[Tensor, " num_gaussians"] = torch.from_numpy(opacities).to("cuda")
+    sh0_t: Float[Tensor, "num_gaussians 1 3"] = torch.from_numpy(sh0).to("cuda")
+    shN_t: Float[Tensor, "num_gaussians S 3"] = torch.from_numpy(shN).to("cuda")
+    colors: Float[Tensor, "num_gaussians S_total 3"] = torch.cat([sh0_t, shN_t], dim=-2).to("cuda")
+
     sh_degree = int(math.sqrt(colors.shape[-2]) - 1)
-    scales = torch.exp(scales)
-    opacities = torch.sigmoid(opacities)
+    scales_t = torch.exp(scales_t)
+    opacities_t = torch.sigmoid(opacities_t)
 
     @torch.no_grad()
-    def render_fn(camera_state: CameraState, img_wh: tuple[int, int]):
+    def render_fn(camera_state: CameraState, img_wh: tuple[int, int]) -> NDArray:
         width, height = img_wh
-        c2w = camera_state.c2w
-        K = camera_state.get_K(img_wh)
-        c2w = torch.from_numpy(c2w).float().to("cuda")
-        K = torch.from_numpy(K).float().to("cuda")
-        viewmat = c2w.inverse()
-
-        render_colors, _, _ = rasterization(
-            means,  # [N, 3]
-            quats,  # [N, 4]
-            scales,  # [N, 3]
-            opacities,  # [N]
+        camera_c2w = camera_state.c2w
+        camera_K = camera_state.get_K(img_wh)
+        c2w: Float[Tensor, "4 4"] = torch.from_numpy(camera_c2w).float().to("cuda")
+        K: Float[Tensor, "3 3"] = torch.from_numpy(camera_K).float().to("cuda")
+        viewmat: Float[Tensor, "4 4"] = torch.linalg.inv(c2w)
+        # We need to separate the type annotation from the unpacking
+        raster_out: tuple[Float[Tensor, "1 H W 4"], Tensor, Tensor] = rasterization(
+            means_t,  # [N, 3]
+            quats_t,  # [N, 4]
+            scales_t,  # [N, 3]
+            opacities_t,  # [N]
             colors,  # [N, S, 3]
             viewmat[None],  # [1, 4, 4]
             K[None],  # [1, 3, 3]
@@ -81,7 +94,8 @@ if __name__ == "__main__":
             # this is to speedup large-scale rendering by skipping far-away Gaussians.
             radius_clip=3,
         )
-        render_rgbs = render_colors[0, ..., 0:3].cpu().numpy()
+        render_colors, _, _ = raster_out
+        render_rgbs: Float[NDArray, "H W 3"] = render_colors[0, ..., 0:3].cpu().numpy()
         return render_rgbs
 
     server = viser.ViserServer(verbose=False)
