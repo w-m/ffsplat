73cd4947c9186339e9576f77c54902ad47041035
Friedrich Branschke <f.branschke@tu-berlin.de>
2025-04-23 14:05:02 +0200

    WIP: encapsulate transformations

--- src/ffsplat/models/operations.py
+++ src/ffsplat/models/operations.py
@@ -1,16 +1,28 @@
 import json
-from dataclasses import dataclass
 from hashlib import sha256
 from typing import TYPE_CHECKING, Any
+
+import ffsplat.models.transformations as transformations
+
+from ..models.transformations import Transformation
 
 if TYPE_CHECKING:
     from ..models.fields import Field
 
 
-@dataclass
 class Operation:
     input_fields: dict[str, "Field"]
     params: dict[str, Any]
+    transform: Transformation
+
+    def __init__(self, input_fields: dict[str, "Field"], params: dict[str, Any], verbose: bool = False) -> None:
+        self.input_fields = input_fields
+        self.params = params
+        # TODO: create transformation based on params
+        self.transform_type = params[next(iter(params))]
+        match self.transform_type:
+            case "cluster":
+                self.transform = transformations.cluster(self.params["cluster"], self)
 
     def __hash__(self) -> int:
         json_str = json.dumps(self.to_json())
@@ -25,3 +37,6 @@ class Operation:
             "input_fields": {name: field.to_json() for name, field in self.input_fields.items()},
             "params": self.params,
         }
+
+    def apply(self):
+        return self.transform.apply()
